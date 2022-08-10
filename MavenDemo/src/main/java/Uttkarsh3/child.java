f8fe6a2a4aa60c946b566fc6e8387f2949d5e9ca
adarshshivam2000 <adarshshivam2000@gmail.com>
2022-08-10 16:17:02 +0530

    first commit

--- MavenDemo/src/main/java/Uttkarsh3/child.java
+++ MavenDemo/src/main/java/Uttkarsh3/child.java
@@ -1,5 +1,13 @@
 package Uttkarsh3;
 
-public class child {
-
+public class child  {
+ public static void main(String[] args) {
+	// adarsh First commit adding printing statement 
+	 System.out.println("First git hub commit");
+	 
 }
+}
+ 
+
+
+
